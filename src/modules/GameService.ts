08a22c1e70840ec506f184d56e6a3de0b662cc59
valery.markunin <valery.markunin@simbirsoft.com>
2025-06-13 23:33:31 +0400

    Client Адрес сервера

--- src/modules/GameService.ts
+++ src/modules/GameService.ts
@@ -33,7 +33,10 @@ class GameService implements IGameService {
       return
     }
 
-    this.socket = io('http://localhost:3001', {
+    this.socket = io('https://karkasson.onrender.com', {
+      secure: true,
+      rejectUnauthorized: true,
+      port: 10000,
       transports: ['websocket'],
       reconnection: true,
       reconnectionAttempts: 10,
