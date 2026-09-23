bda0bd2a991583bf10de93d019ee5d32bc8e98b9
Valery Markunin <khgr867@astrazeneca.net>
2026-09-23 21:28:17 +0400

    Удаление мёртвого кода в GameControls (calculateBestMove и закомментированный блок); opencode.json: добавлены провайдер и агенты Тециус

--- src/components/GameControls.vue
+++ src/components/GameControls.vue
@@ -5,26 +5,13 @@
     :initial-x="600"
     :initial-y="10"
   >
-    <!--    <div class="game-controls">-->
-    <!--      <template v-if="gameBoard.gameIsStarted">-->
-    <!--        <button @click="gameBoard.autoPlay">Auto Play</button>-->
-    <!--        <button @click="calculateBestMove" class="calculate-move-btn">Calculate Best Move</button>-->
-
-    <!--        <button @click="zoomToLastPlacement">Zoom</button>-->
-    <!--        <div class="game-current-player">-->
-    <!--          <div class="game-current-player__title">Ход игрока {{ gameBoard.currentPlayer.name }}</div>-->
-    <!--        </div>-->
-    <!--      </template>-->
-    <!--    </div>-->
     <GameStats :game-board="gameBoard" />
   </Draggable>
 </template>
 <script setup lang="ts">
 import GameStats from '@/components/GameStats.vue'
-import { GameSimulatorModule } from '@server/modules/GameSimulatorModule'
 import Draggable from '@/components/Draggable.vue'
 import type { IGameBoard } from '@/types/game'
-import type { IGameBoard as ServerGameBoard } from '@server/modules/GameManager'
 
 const props = defineProps({
   gameBoard: {
@@ -32,28 +19,4 @@ const props = defineProps({
     required: true,
   },
 })
-
-const calculateBestMove = () => {
-  const board = props.gameBoard as unknown as ServerGameBoard
-  if (!board.currentTile) return
-
-  const tileForSim = {
-    ...board.currentTile,
-    sides: { ...board.currentTile.sides },
-  }
-
-  const simulator = new GameSimulatorModule(board)
-  const result = simulator.findBestMove(tileForSim)
-
-  if (result.score > -1) {
-    const move = result.moves[0]
-    if (move) {
-      board.placeTile(move.tile, move.rowIndex, move.tileIndex)
-
-      if (move.followerPlace) {
-        board.placeFollower(move.followerPlace)
-      }
-    }
-  }
-}
 </script>
